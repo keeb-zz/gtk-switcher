ae911ee949e57b6792c08e555239e45b7816127f
Nick Stinemates <nick@stinemates.org>
2012-08-01 00:35:33 -0700

    implemented gtk refresh in c as you cant in pygtk

---
+++ src/c/refresh.c
@@ -0,0 +1,30 @@
+#include <gtk/gtk.h>
+#include <Python.h>
+
+void send_refresh_signal(void)
+{
+    GdkEventClient event;
+    gtk_init(NULL, NULL);
+    event.type = GDK_CLIENT_EVENT;
+    event.send_event = TRUE;
+    event.window = NULL;
+    event.message_type = gdk_atom_intern("_GTK_READ_RCFILES", FALSE);
+    event.data_format = 8;
+    gdk_event_send_clientmessage_toall((GdkEvent *)&event);
+}
+
+static PyObject* py_refresh(PyObject* self, PyObject* args) {
+    send_refresh_signal();
+    char *s = "success";
+    return Py_BuildValue("s", s);
+}
+
+static PyMethodDef refresh_methods[] = {
+    {"refresh", py_refresh, METH_VARARGS, "Refresh GTK Session"},
+    {NULL, NULL, 0, NULL}
+};
+
+PyMODINIT_FUNC initrefresh() {
+    (void) Py_InitModule("refresh", refresh_methods);
+}
+
